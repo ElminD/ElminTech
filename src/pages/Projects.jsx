27c299af275451a764a28c1a33bde6d20c75c725
Elmin Didic <35606145+ElminD@users.noreply.github.com>
2023-12-01 12:59:31 -0600

    Add Advent of Code 2023 section to Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -18,6 +18,30 @@ const Projects = () => {
       Throughout my professional journey, I've engaged in a diverse range of projects, each presenting unique challenges and opportunities for growth. From tackling complex problem-solving in software development to orchestrating seamless project management in a fast-paced environment, my experience reflects a versatile skill set. Explore the details of these endeavors below to gain a deeper understanding of my hands-on involvement and the valuable lessons learned along the way.
       </p>
 
+      <br></br>
+
+      <h1 className='head-text'>
+        🎄
+        <span className='bg-gradient-to-r from-red-500 via-red-700 to-red-600 inline-block text-transparent bg-clip-text drop-shadow font-semibold'>
+         Advent of Code 2023 
+        </span>
+        🎁
+      </h1>
+
+      <p className='text-slate-500 mt-2 leading-relaxed'>
+      Come check out my Advent of Code 2023 solutions!  
+      <Link
+        to={'https://github.com/ElminD/AdventOfCode2023'}
+        target='_blank'
+        rel='noopener noreferrer'
+        className='font-semibold text-green-700'
+      >
+         ⭐Here!⭐
+      </Link>
+      </p>
+
+
+
       <div className='flex flex-wrap my-20 gap-16'>
         {projects.map((project) => (
           <div className='lg:w-[400px] w-full' key={project.name}>
